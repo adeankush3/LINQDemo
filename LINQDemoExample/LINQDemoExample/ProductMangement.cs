9b4e1cfa1e04426cc59448ed38d00fafef72377a
adeankush3 <adeankush3@gmail.com>
2022-03-16 20:50:50 +0530

    [Ankush]Retrive count of review resent for each productID

--- LINQDemoExample/LINQDemoExample/ProductMangement.cs
+++ LINQDemoExample/LINQDemoExample/ProductMangement.cs
@@ -24,16 +24,31 @@ namespace LINQDemoExample
         }
         public void SelectedRecords(List<ProductReview> listProductReview)
         {
+            //var recordData = from productReview in listProductReview
+            //                 where (productReview.ProductId == 1 && productReview.Rating > 3) ||
+            //                 (productReview.ProductId == 4 && productReview.Rating > 3) ||
+            //                 (productReview.ProductId == 9 && productReview.Rating > 3)
+            //                 select productReview;
+
             var recordData = from productReview in listProductReview
-                             where (productReview.ProductId == 1 && productReview.Rating > 3) ||
-                             (productReview.ProductId == 4 && productReview.Rating > 3) ||
-                             (productReview.ProductId == 9 && productReview.Rating > 3)
+                             where (productReview.ProductId == 1 || productReview.ProductId == 4 || productReview.ProductId == 9)
+                             && productReview.Rating > 3
                              select productReview;
             Console.WriteLine("Selected Record Display Rating");
 
             foreach (var list in recordData)
             {
                 Console.WriteLine("ProductId-" + list.ProductId + "" + "UserId-" + list.UserId + "" + "Rating-" + list.Rating + "" + "Review-" + list.Review + "" + "IsLike-" + list.IsLike);
+            }
+        }
+        public void RetriveCountOfRecord(List<ProductReview> listProductReview)
+        {
+            Console.WriteLine("\n - - -  Retrieve count of review present for each productID - - - ");
+            var recordData = listProductReview.GroupBy(x => x.ProductId).Select(x => new { ProductId = x.Key, count = x.Count() });
+            
+            foreach (var list in recordData)
+            {
+                Console.WriteLine(" Product Id : {0} \t ReviewCount : {1}", list.ProductId, list.count);
             }
         }
     }
--- LINQDemoExample/LINQDemoExample/Program.cs
+++ LINQDemoExample/LINQDemoExample/Program.cs
@@ -15,38 +15,40 @@ namespace LINQDemoExample
             {
                 new ProductReview() { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", IsLike = true },
                 new ProductReview() { ProductId = 2, UserId = 1, Rating = 5, Review = "Good", IsLike = true },
-                new ProductReview() { ProductId = 3, UserId = 2, Rating = 4, Review = "Good", IsLike = false },
+                new ProductReview() { ProductId = 3, UserId = 2, Rating = 4, Review = "Avg", IsLike = false },
                 new ProductReview() { ProductId = 4, UserId = 2, Rating = 5, Review = "Good", IsLike = false },
                 new ProductReview() { ProductId = 5, UserId = 3, Rating = 3, Review = "Good", IsLike = true },
                 new ProductReview() { ProductId = 6, UserId = 4, Rating = 5, Review = "nice", IsLike = false },
                 new ProductReview() { ProductId = 7, UserId = 3, Rating = 2, Review = "Good", IsLike = true },
-                new ProductReview() { ProductId = 8, UserId = 10, Rating = 5, Review = "Good", IsLike = false },
-                new ProductReview() { ProductId = 9, UserId = 10, Rating = 3, Review = "nice", IsLike = true },
+                new ProductReview() { ProductId = 8, UserId = 10, Rating = 5, Review = "Avg", IsLike = false },
+                new ProductReview() { ProductId = 10, UserId = 10, Rating = 3, Review = "nice", IsLike = true },
                 new ProductReview() { ProductId = 10, UserId = 10, Rating = 1, Review = "Good", IsLike = false },
                 new ProductReview() { ProductId = 11, UserId = 10, Rating = 5, Review = "nice", IsLike = true },
-                new ProductReview() { ProductId = 12, UserId = 10, Rating = 4, Review = "Good", IsLike = false },
-                new ProductReview() { ProductId = 13, UserId = 10, Rating = 5, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 12, UserId = 17, Rating = 4, Review = "Good", IsLike = false },
+                new ProductReview() { ProductId = 13, UserId = 8, Rating = 5, Review = "Avg", IsLike = true },
                 new ProductReview() { ProductId = 14, UserId = 10, Rating = 3, Review = "bad", IsLike = false },
-                new ProductReview() { ProductId = 15, UserId = 10, Rating = 2, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 15, UserId = 9, Rating = 2, Review = "Good", IsLike = true },
                 new ProductReview() { ProductId = 16, UserId = 10, Rating = 3, Review = "bad", IsLike = false },
-                new ProductReview() { ProductId = 17, UserId = 10, Rating = 5, Review = "bad", IsLike = true },
+                new ProductReview() { ProductId = 17, UserId = 11, Rating = 5, Review = "bad", IsLike = true },
                 new ProductReview() { ProductId = 18, UserId = 10, Rating = 4, Review = "good", IsLike = true },
-                new ProductReview() { ProductId = 19, UserId = 10, Rating = 5, Review = "nice", IsLike = false },
-                new ProductReview() { ProductId = 20, UserId = 10, Rating = 1, Review = "bad", IsLike = true },
-                new ProductReview() { ProductId = 21, UserId = 10, Rating = 5, Review = "good", IsLike = false },
-                new ProductReview() { ProductId = 22, UserId = 10, Rating = 3, Review = "nice", IsLike = true },
-                new ProductReview() { ProductId = 23, UserId = 10, Rating = 5, Review = "good", IsLike = false },
-                new ProductReview() { ProductId = 24, UserId = 10, Rating = 4, Review = "nice", IsLike = true },
-                new ProductReview() { ProductId = 25, UserId = 10, Rating = 5, Review = "bad", IsLike = false }
+                new ProductReview() { ProductId = 19, UserId = 12, Rating = 5, Review = "nice", IsLike = false },
+                new ProductReview() { ProductId = 20, UserId = 13, Rating = 1, Review = "bad", IsLike = true },
+                new ProductReview() { ProductId = 21, UserId = 14, Rating = 5, Review = "Avg", IsLike = false },
+                new ProductReview() { ProductId = 22, UserId = 15, Rating = 3, Review = "nice", IsLike = true },
+                new ProductReview() { ProductId = 23, UserId = 20, Rating = 5, Review = "good", IsLike = false },
+                new ProductReview() { ProductId = 24, UserId = 21, Rating = 4, Review = "nice", IsLike = true },
+                new ProductReview() { ProductId = 25, UserId = 23, Rating = 5, Review = "bad", IsLike = false }
             };
             foreach (var list in productReviewsList)
             {
                 Console.WriteLine("ProductId-" + list.ProductId + "" + "UserId-" + list.UserId + "" + "Rating-" + list.Rating + "" + "Review-" + list.Review + "" + "IsLike-" + list.IsLike);
             }
             ProductManagement management = new ProductManagement();
-            management.Top3Products(productReviewsList);
-            management.SelectedRecords(productReviewsList);
-            
+            //management.Top3Products(productReviewsList);
+            //management.SelectedRecords(productReviewsList);
+            management.RetriveCountOfRecord(productReviewsList);
+
+
         }
     }
 }
